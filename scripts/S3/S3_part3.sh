4d01076f7ccde278ce3ddaf825cd6a6d7e0b1e16
Daniel Kennedy <djk2120@columbia.edu>
2022-09-07 13:16:15 -0600

    rebuilt outside of scripting

--- scripts/S3/S3_part3.sh
+++ scripts/S3/S3_part3.sh
@@ -12,20 +12,7 @@ module load ncarenv
 ncar_pylib
 
 cd $caseroot
-./xmlchange NTASKS_CPL=-80 #
-./xmlchange NTASKS_LND=-80 # 
-./xmlchange NTASKS_ICE=-80 # 
-./xmlchange NTASKS_OCN=-80 # 
-./xmlchange NTASKS_ROF=-80 #
-./xmlchange NTASKS_GLC=-80 #
-./xmlchange NTASKS_WAV=-80 #
-./xmlchange STOP_N=60
-./xmlchange DATM_CLMNCEP_YR_END=2021
-./case.setup --reset
 
-
-
-./case.build
 
 cp user_nl_datm.1901-2021 user_nl_datm
 cd '/glade/scratch/djk2120/'$casename'/run'
